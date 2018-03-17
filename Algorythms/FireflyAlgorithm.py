cb38de1d7666982aea361ce799b5397d38cf0a89
jakub <jasonekjakub@gmail.com>
2018-03-17 15:02:09 +0100

    PSO Algorithm added.

--- Algorythms/FireflyAlgorithm.py
+++ Algorythms/FireflyAlgorithm.py
@@ -8,21 +8,19 @@ import random
 class FireflyAlgorithm(GenericAlgorithm):
 
     def __init__(self):
-
         self.x_min = -3
         self.x_max = 5
         self.y_min = -3
         self.y_max = 5
         self.steps = 81
         self.n_flies = 80
-        self.delta = -2.4
+        self.delta = -0.2
         self.rand_wander = 0.6
 
         self.fig, self.ax = plt.subplots()
         self.back = None
         self.ln = None
         self.flies = []
-
         super().__init__()
 
     def update(self, frame):
@@ -47,35 +45,37 @@ class FireflyAlgorithm(GenericAlgorithm):
 
         x = np.linspace(self.x_min, self.x_max, self.steps)
         y = np.linspace(self.y_min, self.y_max, self.steps)
-        z = np.array([((1 - i) ** 2 + 100 * (j - i ** 2) ** 2) for j in y for i in x])
+        z = np.array([self.function(i, j) for j in y for i in x])
 
         X, Y = np.meshgrid(x, y)
         Z = z.reshape(self.steps, self.steps)
 
         for i in range(0, self.n_flies):
-            self.flies.append(Firefly(self.x_min, self.x_max, self.y_min, self.y_max, self.rand_wander))
+            self.flies.append(Firefly(self.x_min, self.x_max, self.y_min, self.y_max, self.rand_wander, self.function))
 
         self.back = plt.pcolormesh(X, Y, Z)
         self.ln, = plt.plot([fly.x for fly in self.flies], [fly.y for fly in self.flies], 'wo',
                             animated=True, markersize=1)
-
         return self.ln,
 
     def start(self):
-        self.animation = FuncAnimation(self.fig, self.update, frames=None,
-                    init_func=self.init, blit=True)
+        self.animation = FuncAnimation(self.fig, self.update, frames=None, init_func=self.init, blit=True)
+
+    def function(self, x, y):
+        return ((1 - x) ** 2 + 100 * (y - x ** 2) ** 2)
 
 
 class Firefly:
 
-    def __init__(self, x_min, x_max, y_min, y_max, r):
+    def __init__(self, x_min, x_max, y_min, y_max, r, f):
         self.x = random.uniform(x_min, x_max)
         self.y = random.uniform(y_min, y_max)
         self.lightness = ((1 - self.x) ** 2 + 100 * (self.y - self.x ** 2) ** 2)
         self.rand_wander = r
+        self.f = f
 
     def adjust_lightness(self):
-        self.lightness = ((1 - self.x) ** 2 + 100 * (self.y - self.x ** 2) ** 2)
+        self.lightness = self.f(self.x, self.y)
 
     def move_to(self, other_firefly):
         r = random.gauss(0, self.rand_wander)
@@ -89,6 +89,7 @@ class Firefly:
         r = random.gauss(-self.rand_wander, self.rand_wander)
         self.x = self.x + r
         self.y = self.y + r
+
 
 if __name__ == "__main__":
     FfA = FireflyAlgorithm()
--- Algorythms/PSOAlgorithm.py
+++ Algorythms/PSOAlgorithm.py
@@ -1,26 +1,101 @@
 from Algorythms.GenericAlgorithm import GenericAlgorithm
+import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+import random
 
 
 class PSOAlgorithm(GenericAlgorithm):
 
     def __init__(self):
-        pass
+        self.x_min = -3
+        self.x_max = 5
+        self.y_min = -3
+        self.y_max = 5
+        self.steps = 81
+        self.n_particles = 80
+
+        self.best_x = 0
+        self.best_y = 0
+        self.fig, self.ax = plt.subplots()
+        self.back = None
+        self.ln = None
+        self.particles = []
+        super().__init__()
 
     def update(self, frame):
-        pass
+
+        for particle in self.particles:
+            particle.update_velocity(self.best_x, self.best_y)
+            particle.move()
+            particle.check_best()
+            if particle.f(particle.x, particle.y) < self.function(self.best_x, self.best_y):
+                self.best_x = particle.x
+                self.best_y = particle.y
+
+        self.ln, = plt.plot([particle.x for particle in self.particles], [particle.y for particle in self.particles],
+                        'wo', animated=True, markersize=1)
+        return self.ln,
 
     def init(self):
-        pass
+        self.ax.set_xlim(self.x_min, self.x_max)
+        self.ax.set_ylim(self.y_min, self.y_max)
+
+        x = np.linspace(self.x_min, self.x_max, self.steps)
+        y = np.linspace(self.y_min, self.y_max, self.steps)
+        z = np.array([self.function(i, j) for j in y for i in x])
+
+        X, Y = np.meshgrid(x, y)
+        Z = z.reshape(self.steps, self.steps)
+
+        for i in range(0, self.n_particles):
+            self.particles.append(Particle(self.x_min, self.x_max, self.y_min, self.y_max, self.function))
+            if i == 0:
+                self.best_x = self.particles[i].x
+                self.best_y = self.particles[i].y
+            else:
+                if self.function(self.particles[i].x, self.particles[i].y) < self.function(self.best_x, self.best_y):
+                    self.best_x = self.particles[i].x
+                    self.best_y = self.particles[i].y
+
+        self.back = plt.pcolormesh(X, Y, Z)
+        self.ln, = plt.plot([particle.x for particle in self.particles], [particle.y for particle in self.particles],
+                            'wo', animated=True, markersize=1)
+        return self.ln,
 
     def start(self):
-        pass
+        self.animation = FuncAnimation(self.fig, self.update, frames=None, init_func=self.init, blit=True)
+
+    def function(self, x, y):
+        return (1 - x) ** 2 + 100 * (y - x ** 2) ** 2
 
 
 class Particle:
-    def __init__(self):
-        pass
+
+    def __init__(self, x_min, x_max, y_min, y_max, f):
+        self.x = random.uniform(x_min, x_max)
+        self.y = random.uniform(y_min, y_max)
+        self.best_x = self.x
+        self.best_y = self.y
+        self.v_x = random.uniform(-(x_max - x_min), (x_max - x_min))
+        self.v_y = random.uniform(-(y_max - y_min), (y_max - y_min))
+        self.f = f
+
+    def update_velocity(self, global_best_x, global_best_y):
+        r1 = random.uniform(0, 1)
+        r2 = random.uniform(0, 1)
+
+        self.v_x = 0.75 * self.v_x + 1.32 * r1 * (self.best_x - self.x) + 1.32 * r2 * (global_best_x - self.x)
+        self.v_y = 0.75 * self.v_y + 1.32 * r1 * (self.best_y - self.y) + 1.32 * r2 * (global_best_y - self.x)
+
+    def move(self):
+        self.x = self.x + self.v_x
+        self.y = self.y + self.v_y
+
+    def check_best(self):
+        if self.f(self.x, self.y) < self.f(self.best_x, self.best_y):
+            self.best_x = self.x
+            self.best_y = self.y
 
 
 if __name__ == "__main__":
